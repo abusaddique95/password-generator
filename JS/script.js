11a869be977e710831c10a501c6fa6caa8af5d00
abusaddique95 <abusaddique95@live.com>
2022-04-20 18:15:52 +0100

    added length to password and the criteria to be matched

--- JS/script.js
+++ JS/script.js
@@ -2,20 +2,66 @@
 const generateBtn = document.querySelector("#generate");
 
 const getPasswordLength = () => {
-  return 10;
+  const length = parseInt(
+    prompt("What length would you like your password to be?")
+  );
+
+  // Research best way to check if variable is a number
+  // Add conditional where we alert user that it should be a number typed
+
+  // Conditional to check if length is less than 8 characters
+  // Alert user that should be at least 8 if lower than 8
+
+  // Same as above but for less than 128
+
+  return length;
 };
 
 const getPasswordCriteria = () => {
-  return [
-    "abcdefghijklmnopqrstuvwxyz",
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-    "0123456789",
-    " !\"#$%&'()*+,-./:;<=>?@[]^_`{|}~",
-  ];
+  //Declare an empty array where we can add values as they are selected
+  let includedPasswordValues = [];
+
+  //Declare string values for different types of characters
+  const lowerCaseValues = "abcdefghijklmnopqrstuvwxyz";
+
+  const upperCaseValues = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+  const specialCharacterValues = "!\"#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+
+  const numberValues = "0123456789";
+
+  //Use confirms to find out which characters are wanted
+  const useLowerCase = confirm("Would you like lower case characters?");
+
+  if (useLowerCase) {
+    includedPasswordValues.push(lowerCaseValues);
+  }
+
+  const useUpperCase = confirm("Would you like upper case characters?");
+
+  if (useUpperCase) {
+    includedPasswordValues.push(upperCaseValues);
+  }
+
+  const useSpecials = confirm("Would you like Special characters?");
+
+  if (useSpecials) {
+    includedPasswordValues.push(specialCharacterValues);
+  }
+
+  const useNumbers = confirm("Would you like numbers?");
+
+  if (useNumbers) {
+    includedPasswordValues.push(numberValues);
+  }
+
+  console.log(includedPasswordValues);
+  //Return the array with the strings of characters requested
+  return includedPasswordValues;
 };
 
 const createRandomPassword = () => {
-  return "kdUE28(@d0";
+  return "kdUE284(@d0";
 };
 
 // main function to generate the random password
