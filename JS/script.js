ca15e2057ceb2fdf20a6ffd0a4d5d55fac9a2798
abusaddique95 <abusaddique95@live.com>
2022-04-20 16:18:09 +0100

    linked starter code and linked correctly

---
+++ JS/script.js
@@ -0,0 +1,44 @@
+// Assignment Code
+const generateBtn = document.querySelector("#generate");
+
+const getPasswordLength = () => {
+  return 10;
+};
+
+const getPasswordCriteria = () => {
+  return [
+    "abcdefghijklmnopqrstuvwxyz",
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    "0123456789",
+    " !\"#$%&'()*+,-./:;<=>?@[]^_`{|}~",
+  ];
+};
+
+const createRandomPassword = () => {
+  return "kdUE28(@d0";
+};
+
+// main function to generate the random password
+const generatePassword = () => {
+  // get the password length
+  const passwordLength = getPasswordLength();
+
+  // get the password criteria
+  const passwordCriteria = getPasswordCriteria();
+
+  // create random password
+  const password = createRandomPassword(passwordLength, passwordCriteria);
+
+  return password;
+};
+
+// Write password to the #password input
+const writePassword = () => {
+  const password = generatePassword();
+  const passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+};
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
